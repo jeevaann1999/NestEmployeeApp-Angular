142f8a247ae260b7a62b90d0e8c2dff81f7a0d9c
jeevaann1999 <jeevaann1999@gmail.com>
2022-12-12 05:44:43 +0530

    changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+
+const myRoute:Routes=[
+  {
+    
+  }
+]
 
 @NgModule({
   declarations: [
@@ -10,7 +19,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(),
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
